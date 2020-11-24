274a5810547516d0f68ece3808db569044ef6a76
Lucas Jung <lucasjungdeveloper@gmail.com>
2020-11-23 20:46:30 -0400

    Finished

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,56 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1 class="is-size-2">Pokedex</h1>
+    <div class="column is-half is-offset-one-quarter">
+      <input
+        type="text"
+        placeholder="Search a Pokemon"
+        v-model="search"
+        class="input is-rounded"
+      />
+      <button class="button is-fullwidth is-success" style="margin-top: 1%">
+        Search
+      </button>
+      <div v-for="(pokemon, index) in searchResult" :key="pokemon.url">
+        <Pokemon :name="pokemon.name" :url="pokemon.url" :num="index + 1" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import axios from "axios";
+import Pokemon from "./components/Pokemon";
 export default {
-  name: 'App',
+  name: "App",
+  data() {
+    return {
+      pokemons: [],
+      search: "",
+    };
+  },
+  created: function() {
+    axios
+      .get("https://pokeapi.co/api/v2/pokemon?limit=152&offset=0")
+      .then((res) => {
+        this.pokemons = res.data.results;
+      });
+  },
   components: {
-    HelloWorld
-  }
-}
+    Pokemon,
+  },
+  computed: {
+    searchResult: function() {
+      if (this.search.trim().length == 0) {
+        return this.pokemons;
+      } else {
+        return this.pokemons.filter(
+          (pokemon) => pokemon.name == this.search.trim().toLowerCase()
+        );
+      }
+    },
+  },
+};
 </script>
 
 <style>
